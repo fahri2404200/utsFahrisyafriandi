52a5010a845fcd10b523c58749d7d8ef39697eaa
fahri2404200 <49123071+fahri2404200@users.noreply.github.com>
2019-03-31 22:31:21 +0800

    Add files via upload

---
+++ soal1wajib.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"nama : fahri syafriandi K"<<endl;
+    cout<<"nim  : 1829142008       "<<endl;
+    cout<<"kelas: teknik komputer B"<<endl;
+    cout<<"tahun angkatan : 2018  "<<endl;
+
+    return 0;
+}
